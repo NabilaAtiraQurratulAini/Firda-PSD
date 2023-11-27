ddefe46bd4bd5dbc04d31ca6f407cb71a688d275
NabilaAtiraQurratulAini <107897951+NabilaAtiraQurratulAini@users.noreply.github.com>
2023-11-27 08:05:52 +0700

    psdproject2.py

--- psdproject2.py
+++ psdproject2.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-#import scipy.stats
-import librosa
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import accuracy_score
